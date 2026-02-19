64716219627ef19d860224d9d8da4f154862352f
zach <zacharykeef6767@gmail.com>
2026-02-19 21:39:03 +0000

    Fix manifest and service worker paths for proper relative resolution

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'mc-coords-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
